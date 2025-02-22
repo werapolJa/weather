d4fd55a07752108b59429367d08a44eaccf1c3a0
werapol <wesakaber@gmail.com>
2025-02-22 13:44:22 +0700

    fix:bug bgcolor gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,13 +40,13 @@ export default function Home() {
 
       const celsiusTemp = Math.round(res.data.main.temp - 273.15);
       if (celsiusTemp < 20) {
-        setBgGradient("bg-gradient-to-b from-sky-300 to-purple-300");
+        setBgGradient("bg-sky-300");
         console.log("เย็น");
       } else if (celsiusTemp >= 20 && celsiusTemp <= 25) {
-        setBgGradient("bg-gradient-to-b from-emerald-300 to-sky-300");
+        setBgGradient("bg-emerald-300");
         console.log("อุ่น");
       } else if (celsiusTemp > 25) {
-        setBgGradient("bg-gradient-to-b from-rose-300 to-purple-300");
+        setBgGradient("bg-rose-300");
         console.log("ร้อน");
       }
     } catch (error) {
@@ -55,32 +55,34 @@ export default function Home() {
   };
 
   return (
-    <main className="max-w-2xl mx-auto select-none">
-      <div className="flex justify-center items-center flex-col pt-52 mx-10">
-        <h1 className="text-3xl md:text-6xl font-bold mb-3 text-white">
-          Weather App
-        </h1>
-        <h3 className="text-base md:text-xl text-slate-100 mb-5 font-semibold">
-          Enter a city name to check the current weather conditions.
-        </h3>
-        <div className="w-full gap-2 md:flex">
-          <input
-            type="text"
-            placeholder="Enter city name..."
-            className="w-full rounded-full px-5 border-2 py-2 mb-2 md:mb-0"
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
-          />
-          <button
-            className="px-4 py-2 bg-blue-400 rounded-full text-white flex justify-center md:items-center w-full md:w-auto border-0 focus:outline-none"
-            onClick={() => seSerachquery(searchInput)}
-          >
-            Search
-          </button>
+    <main className="w-full h-auto  "> 
+      <div className="max-w-2xl mx-auto select-none ">
+        <div className="flex justify-center items-center flex-col pt-20  mx-10 md:pt-52 mn">
+          <h1 className="text-3xl md:text-6xl font-bold mb-3 text-white">
+            Weather App
+          </h1>
+          <h3 className="text-base md:text-xl text-slate-100 mb-5 font-semibold">
+            Enter a city name to check the current weather conditions.
+          </h3>
+          <div className="w-full gap-2 md:flex">
+            <input
+              type="text"
+              placeholder="Enter city name..."
+              className="w-full rounded-full px-5 border-2 py-2 mb-2 md:mb-0"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+            <button
+              className="px-4 py-2 bg-blue-400 rounded-full text-white flex justify-center md:items-center w-full md:w-auto border-0 focus:outline-none"
+              onClick={() => seSerachquery(searchInput)}
+            >
+              Search
+            </button>
+          </div>
+          {weather && <WeatherCard weather={weather} />}
+          {/* แสดงอุณหภูมิที่คำนวณแล้ว */}
+          {temperature && <p>Temperature: {temperature}</p>}
         </div>
-        {weather && <WeatherCard weather={weather} />}
-        {/* แสดงอุณหภูมิที่คำนวณแล้ว */}
-        {temperature && <p>Temperature: {temperature}</p>}
       </div>
     </main>
   );
